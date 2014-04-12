8fd736337815ec73488d56cad3a96eadd0ec7680
fowlmouth <phowl.mouth@gmail.com>
2014-04-11 19:39:08 -0500

    updated the lib name

--- enet.nim
+++ enet.nim
@@ -24,7 +24,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 when defined(Linux):
-  const Lib = "libenet.so.1(|.0.3)"
+  const Lib = "libenet.so"#.1(|.0.3)"
 else:
   {.error: "Your platform has not been accounted for."}
 {.deadCodeElim: ON.}
